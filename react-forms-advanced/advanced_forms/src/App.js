182e9cc0b50ce87a9b22785fe7a641e74f365e04
Justin <jcdiner@gmail.com>
2023-03-22 16:14:40 -0400

    testing

---
+++ react-forms-advanced/advanced_forms/src/App.js
@@ -0,0 +1,12 @@
+import Title from './components/TitleComponent/index'
+
+function App() {
+  return (
+		<>
+			<h1>Hello from App</h1>
+			<Title />
+		</>
+  );
+}
+
+export default App;
---
+++ react-forms-advanced/advanced_forms/src/components/TitleComponent/index.js
@@ -0,0 +1,7 @@
+const Title = () => {
+	return (
+		<img className="logo" src='../../../_assets/logo.png'></img>
+	)
+}
+
+export default Title; 
